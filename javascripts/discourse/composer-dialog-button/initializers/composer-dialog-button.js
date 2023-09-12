7c01be42ae47a78ec586d62ab19c5d21ebb08665
denvergeeks <jen@denvergeeks.com>
2023-09-12 11:08:33 -0600

    Update composer-dialog-button.js

--- javascripts/discourse/composer-dialog-button/initializers/composer-dialog-button.js
+++ javascripts/discourse/composer-dialog-button/initializers/composer-dialog-button.js
@@ -2,7 +2,7 @@ import { withPluginApi } from "discourse/lib/plugin-api";
 import I18n from "I18n";
 
 export default {
-  name: "composer-footnote-button",
+  name: "composer-dialog-button",
 
   initialize() {
     withPluginApi("0.8", (api) => {   
@@ -11,18 +11,18 @@ export default {
         I18n.translations[currentLocale].js.composer = {};
       }
       
-      I18n.translations[currentLocale].js.footnote_button_title = I18n.t(themePrefix("composer_footnote_button_title"));
-      I18n.translations[currentLocale].js.composer.footnote_button_text = I18n.t(themePrefix("composer_footnote_button_text"));
+      I18n.translations[currentLocale].js.dialog_button_title = I18n.t(themePrefix("composer_dialog_button_title"));
+      I18n.translations[currentLocale].js.composer.dialog_button_text = I18n.t(themePrefix("composer_dialog_button_text"));
       
       api.modifyClass("controller:composer", {
-        pluginId: "FootnoteButton",
+        pluginId: "DialogButton",
 
         actions: {
-          footnoteButton() {
+          dialogButton() {
             this.get("toolbarEvent").applySurround(
-              '^[',
-              "]",
-              "footnote_button_text",
+              '<dialog>',
+              "</dialog>",
+              "dialog_button_text",
               { multiline: false }
             );
           },
@@ -32,24 +32,24 @@ export default {
       if (settings.put_in_popup_menu) {
         api.addToolbarPopupMenuOptionsCallback((controller) => {
           return {
-            action: "footnoteButton",
-            icon: settings.composer_footnote_button_icon,
-            label: "footnote_button_title",
+            action: "dialogButton",
+            icon: settings.composer_dialog_button_icon,
+            label: "dialog_button_title",
           };
         });
       } else {
         api.onToolbarCreate(function(toolbar) {
           toolbar.addButton({
             trimLeading: true,
-            id: "quick-footnote",
-            group: settings.composer_footnote_button_group,
-            icon: settings.composer_footnote_button_icon,
-            title: "footnote_button_title",
+            id: "quick-dialog",
+            group: settings.composer_dialog_button_group,
+            icon: settings.composer_dialog_button_icon,
+            title: "dialog_button_title",
             perform: function(e) {
               return e.applySurround(
-                '^[',
-                "]",
-                "footnote_button_text",
+                '<dialog>',
+                "</dialog>",
+                "dialog_button_text",
                 { multiline: false }
               );
             }
